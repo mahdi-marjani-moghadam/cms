481fe7f4ab61fcf66d0343e575b30d2dafc7523d
mahdi moghadam <marjani.mahdi@gmail.com>
2025-05-02 16:34:04 +0330

    before tailwind update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,49 +1,33 @@
-
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 var LiveReloadPlugin = require('webpack-livereload-plugin');
-
 const path = require("path");
-
-const dotenv = require('dotenv').config({
-    path: path.join(__dirname, '.env')
-});
-
+const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 mix.webpackConfig({
     plugins: [new LiveReloadPlugin()],
-    stats: {
-        children: true
-    }
-});
-
-mix.options({
-    processCssUrls: false,
-    postCss: [tailwindcss('./tailwind.config.js')],
+    stats: { children: true }
 });
 
 var template = dotenv.parsed.TEMPLATE_NAME;
 
-mix.setPublicPath('public/' + template + '/')
-    .sass('resources/css/' + template + '.scss', '/', {
-        sassOptions: {
-            strictMath: true
-        }
-    })
-    .copy(
-        'resources/fonts',
-        'public/' + template + '/fonts'
-    )
-    .sass('resources/css/panel.scss', 'panel')
-
-    .copy(
-        'node_modules/@fortawesome/fontawesome-free/webfonts/',
-        'public/' + template + '/panel/webfonts/'
-    )
-    .copy(
-        'resources/js/jquery-3.6.0.min.js',
-        'public/' + template + '/'
-    )
-    // .minify('public/'+template + '/'+template + '.css')
-    .version();
-
+if (template = 'it-times-store') {
+    mix.setPublicPath('public/' + template + '/')
+        .postCss('resources/css/' + template + '.css', '/', {})
+        .copy('resources/fonts', 'public/' + template + '/fonts')
+        .version();
+} else {
+    mix.options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
+    mix.setPublicPath('public/' + template + '/')
+        .sass('resources/css/' + template + '.scss', '/', {
+            sassOptions: { strictMath: true }
+        })
+        .copy('resources/fonts', 'public/' + template + '/fonts')
+        .sass('resources/css/panel.scss', 'panel')
+        .copy('node_modules/@fortawesome/fontawesome-free/webfonts/', 'public/' + template + '/panel/webfonts/')
+        .copy('resources/js/jquery-3.6.0.min.js', 'public/' + template + '/')
+        .version();
+}
